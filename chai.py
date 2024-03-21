02fbbc3ab1ee55a63de861a233f115bbafce1e16
upandey0 <rubusiness0@gmail.com>
2024-03-21 11:44:49 +0530

    Hello Committed

---
+++ chai.py
@@ -0,0 +1,6 @@
+from hello import chai 
+
+chai("Ginger Tea")
+
+# Import hua to kaand ho gya --- __pycache__ folder apne aap ban gya.
+# Kuyn bna wo :  
---
+++ hello.py
@@ -0,0 +1,6 @@
+print("Hello World in Python")
+
+def chai(n):
+    print(n)
+
+chai(4)
